0a8f6ea7697b7160416bfbe55dbf67a6ebfeef5c
joeldavidsalvatierra <joe_sal@hotmail.com.ar>
2016-11-14 11:46:23 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,9 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+	//laventana alert(),muestra mensaje emergente
+	alert("hola");
+	//literarl,mensaje de texto
+	alert("esto funciona de maravilla");
+}
+
---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{//iniciacion simple de una variable
+	var apellido;
+	apellido="Salvatierra"
+	var nombre;
+	nombre="joel";
+	nombre=prompt("ingrese su nombre","N N");
+	alert("mi nombre es: "+nombre+" y su apellido es: "+apellido );
+	/*cuando escribo la var(variable) y despues alert entre ()sale lo que escribi*/
+	//Prompt siempre tiene Nombe= Nombre=prompt()""
+}
+
