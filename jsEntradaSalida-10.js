c78fd2506126b601778e1b7ccede4c31871affb3
joeldavidsalvatierra <joe_sal@hotmail.com.ar>
2016-11-18 16:36:44 -0300

    Add files via upload

--- jsEntradaSalida-10.js
+++ jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var importe;
+	var resultadoFinal;
+	importe=document.getElementById('importe').value;
+	/*resultadoUno=importe*0.25;
+	resultadoFinal=importe-resultadoUno*/
+	resultadoFinal=importe*0.75;
+	document.getElementById('resultado').value=resultadoFinal;
+
 	
 }
