7a1c9a460eafadc11d89a6ba9a3d83b4d5d65af9
joeldavidsalvatierra <joe_sal@hotmail.com.ar>
2016-11-19 19:35:51 -0300

    Add files via upload

--- jsEntradaSalida-1.js
+++ jsEntradaSalida-1.js
@@ -1,9 +1,52 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	//laventana alert(),muestra mensaje emergente
-	alert("hola");
-	//literarl,mensaje de texto
-	alert("esto funciona de maravilla");
+	var elCuadro;
+	elCuadro=document.getElementById('elNombre').value;
+	alert(elCuadro);
 }
-
+{	var importe;
+	var multi;
+	importe=prompt("Ingrese el iporte");
+	multi=importe*1.21;
+	alert("Precio final "+multi);
+}
+{
+	var largo;
+	var ancho;
+	var suma;
+	var multi;
+	largo=document.getElementById('largoTerreno').value;
+	ancho=document.getElementById('anchoTerreno').value;
+	suma=parseInt(largo)+parseInt(ancho);
+	multi=suma*5;
+	alert("Necesita "+multi+" metros de almbre.");
+}
+//recuperatorio
+{
+	var suNombre;
+	suNombre=document.getElementById('elNombre').value;
+	alert(suNombre)
+}
+{
+	var importeUno;
+	var descuento;
+	var resultado;
+	importeUno=prompt("Ingrese la suma");
+	descuento=importeUno*0.25;
+	resultado=importeUno-descuento;
+	alert(resultado);
+}
+{
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+	var promedio;
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+	suma=parseInt(precioUno)+parseInt(precioDos)+parseInt(precioTres);
+	promedio=suma/3;
+	alert("Total a pagar: $"+suma+" el promedio de los precios es "+promedio);
+}
