b9833a951f5001bdd2ab05f5bf228ffdb2c3935d
joeldavidsalvatierra <joe_sal@hotmail.com.ar>
2016-11-22 16:22:10 -0300

    Add files via upload

---
+++ jsInstruccionIF (10).js
@@ -0,0 +1,24 @@
+function Mostrar()
+{
+	//Genero el número RANDOM entre 1 y 10 
+	var random;
+	random=Math.floor((Math.random()*10)+1);
+	console.log(random);
+	if(random>=9)
+	{
+		alert("Exelente");
+	}
+	else
+	{
+		if(random>=4)
+		{
+			alert("Aprobó");
+		}
+	}
+	if(random<4)
+	{
+	alert("Vamos, la proxima se puede");	
+	}
+	
+
+}//FIN DE LA FUNCIÓN
